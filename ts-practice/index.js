ef2ae492c41c156c6f4447160e5cd100c310dae9
rytherlm <aidanryther15@gmail.com>
2023-10-23 09:22:56 -0400

    This is a practice folder, ts-class

---
+++ ts-practice/index.js
@@ -0,0 +1,1 @@
+let word = "aidan was here";
---
+++ video-proccesing-service/src/index.ts
@@ -0,0 +1,15 @@
+import expres from "express";
+import ffmpeg from 'fluent-ffmpeg';
+
+const app = expres();
+const port = 3000;
+
+
+// app.get("/",(req,res) => {
+//     res.send("hello world")
+// });
+
+app.get
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
